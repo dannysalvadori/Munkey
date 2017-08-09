438475d1c1e0daa8ad3515095a506baad27409d4
Danny Salvadori <danny.salvadori@fdmgroup.com>
2017-08-09 18:54:26 +0100

    Added Room entity, service and test

---
+++ HotelBooker/src/main/java/com/fdmgroup/entity/Room.java
@@ -0,0 +1,104 @@
+package com.fdmgroup.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Rooms")
+public class Room {
+
+	@Id
+	@Column
+	private Integer id;
+
+	@ManyToOne(optional=false, targetEntity=Hotel.class) 
+    @JoinColumn(name="hotel_id", nullable=false, updatable=false)
+	private Hotel hotel;
+	
+	@Column
+	private Integer capacity;
+	
+	@Column(name="double_beds")
+	private Integer doubleBeds;
+	
+	@Column(name="single_beds")
+	private Integer singleBeds;
+	
+	@Column(name="price_per_night")
+	private double pricePerNight;
+	
+	@Column(name="price_per_person")
+	private double pricePerPerson;
+	
+	// Copies non-Id column values from a given User object
+	public void clone(Room room) {
+		this.hotel = room.getHotel();
+		this.capacity = room.getCapacity();
+		this.doubleBeds = room.getDoubleBeds();
+		this.singleBeds = room.getSingleBeds();
+		this.pricePerNight = room.getPricePerNight();
+		this.pricePerPerson = room.getPricePerPerson();
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Hotel getHotel() {
+		return hotel;
+	}
+
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
+
+	public Integer getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(Integer capacity) {
+		this.capacity = capacity;
+	}
+
+	public Integer getDoubleBeds() {
+		return doubleBeds;
+	}
+
+	public void setDoubleBeds(Integer doubleBeds) {
+		this.doubleBeds = doubleBeds;
+	}
+
+	public Integer getSingleBeds() {
+		return singleBeds;
+	}
+
+	public void setSingleBeds(Integer singleBeds) {
+		this.singleBeds = singleBeds;
+	}
+
+	public double getPricePerNight() {
+		return pricePerNight;
+	}
+
+	public void setPricePerNight(double pricePerNight) {
+		this.pricePerNight = pricePerNight;
+		if (capacity > 0) {
+			this.pricePerPerson = pricePerNight / capacity; 
+		}
+	}
+
+	public double getPricePerPerson() {
+		return pricePerPerson;
+	}
+
+	public void setPricePerPerson(double pricePerPerson) {
+		this.pricePerPerson = pricePerPerson;
+		if (capacity > 0) {
+			this.pricePerNight = pricePerPerson * capacity; 
+		}
+	}
+	
+}
---
+++ HotelBooker/src/test/java/com/fdmgroup/serviceTest/RoomServiceTest.java
@@ -0,0 +1,121 @@
+package com.fdmgroup.serviceTest;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.fdmgroup.entity.Hotel;
+import com.fdmgroup.entity.Room;
+import com.fdmgroup.service.HotelService;
+import com.fdmgroup.service.RoomService;
+
+public class RoomServiceTest {
+	
+	private static RoomService roomService;
+	private static HotelService hotelService;
+	private static int roomId = 0;
+	private static Hotel testHotel;
+	
+	@BeforeClass
+	public static void setupClass(){
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hb_persistence_unit");
+		roomService = new RoomService(emf);
+		hotelService = new HotelService(emf);
+		
+		testHotel = new Hotel();
+		testHotel.setId(5);
+		hotelService.persistHotel(testHotel);
+	}
+
+	@Test
+	public void persistRoomInsertsNewUserIntoDatabaseTest() {
+		roomId++;
+		Room testRoom = createRoom(roomId);
+		Integer testCapacity = testRoom.getCapacity();
+		roomService.persistRoom(testRoom);
+		
+		Room retrievedRoom = roomService.findRoom(roomId);
+		Integer retrievedCapacity = retrievedRoom.getCapacity();
+		assertEquals(testCapacity, retrievedCapacity);
+	}
+	
+	@Test
+	public void findRoomsByHotelGetsRoomsIfFoundTest() {
+		roomId++;
+		
+		testHotel = new Hotel();
+		testHotel.setId(99);
+		hotelService.persistHotel(testHotel);
+		
+		Room testRoom= createRoom(roomId);
+		testRoom.setHotel(testHotel);
+		Integer expectedId = testRoom.getId();
+		roomService.persistRoom(testRoom);
+		
+		List<Room> retrievedRoomList = roomService.findRoomsByHotel(testHotel);
+		assertEquals(1, retrievedRoomList.size());
+		assertEquals(expectedId, retrievedRoomList.get(0).getId());
+	}
+	
+	@Test
+	public void findRoomsByHotelReturnsEmptyListIfNoneFoundTest() {
+		roomId++;
+		testHotel = new Hotel();
+		testHotel.setId(100);
+		hotelService.persistHotel(testHotel);
+		
+		List<Room> retrievedRoomList = roomService.findRoomsByHotel(testHotel);
+		assertEquals(0, retrievedRoomList.size());
+	}
+
+	@Test
+	public void updateRoomAltersTheDetailsOfAnExistingRoomTest() {
+		roomId++;
+		Room testRoom = createRoom(roomId);
+		roomService.persistRoom(testRoom);
+		
+		Integer newCapacity = 5;
+		testRoom.setCapacity(newCapacity);
+		roomService.updateRoom(testRoom);
+		
+		Room retrievedRoom = roomService.findRoom(roomId);
+		assertEquals(newCapacity, retrievedRoom.getCapacity());
+	}
+	
+	@Test
+	public void removeRoomDeletesRoomFromDatabaseTest() {
+		roomId++;
+		Room testRoom = createRoom(roomId);
+		roomService.persistRoom(testRoom);
+		
+		// Check Room was created successfully in the first place
+		Room retrievedRoom = roomService.findRoom(roomId);
+		assert(retrievedRoom != null);
+		
+		roomService.removeRoom(roomId);
+		Room deletedReservation = roomService.findRoom(roomId);
+		assert(deletedReservation == null);
+	}
+	
+	/**
+	 * Creates a sample Room instance
+	 * @param id
+	 * @return a new instance of Room with pre-defined column values  
+	 */
+	private Room createRoom(int id) {
+		Room r = new Room();
+		r.setId(id);
+		r.setHotel(testHotel);
+		r.setCapacity(4);
+		r.setDoubleBeds(2);
+		r.setSingleBeds(0);
+		r.setPricePerNight(40);
+		return r;
+	}
+
+}
